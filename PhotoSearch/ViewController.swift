e44ea0e0ede65d94230e663f691dae8ab0f81988
Aries Yang <ariesssyyyang@gmail.com>
2020-08-22 01:02:54 +0800

    Create a new project

---
+++ PhotoSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoSearch
+//
+//  Created by Aries Yang on 2020/8/22.
+//  Copyright © 2020 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
